e89c34d85ee07b9958b6572bbb7c6511bf930b74
ali ahmed <alidev338@gmail.com>
2026-02-03 03:17:47 +0200

    make app server-less

--- part3/phonebookbackend/index.js
+++ part3/phonebookbackend/index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const morgan = require("morgan");
 const path = require("path");
-
 const cors = require("cors");
+const serverless = require("serverless-http");
+
 
 const app = express();
 app.use(cors());
@@ -95,7 +96,7 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
-const PORT = process.env.PORT || 3001;
+//const PORT = process.env.PORT || 3001;
 
 // app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-module.exports = app;
+module.exports = serverless(app);
