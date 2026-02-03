a4fe3438f6ad39d274822a92dfe97066b24aaa60
ali ahmed <alidev338@gmail.com>
2026-02-03 02:25:54 +0200

    edit vercel.json

--- part3/phonebookbackend/index.js
+++ part3/phonebookbackend/index.js
@@ -90,4 +90,5 @@ app.get("/api/info", (request, response) => {
 
 const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+module.exports = app;
